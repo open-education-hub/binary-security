34c2addd1111f605d1776790935ab78a91860210
Stefan Jumarea <stefanjumarea02@gmail.com>
2023-06-28 14:53:45 +0300

    bypassing-mitigations: Add solutions and source files
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>

---
+++ bypassing-mitigations/activities/07-challenge-bypass-dep-system-sh-separate/sol/vuln.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const char sh[] = "/bin/sh";
+
+static void hidden(void)
+{
+	system("ls");
+}
+
+static void get_name(void)
+{
+	char buffer[48];
+
+	printf("What's your name? ");
+	fgets(buffer, 48, stdin);
+	if (buffer[strlen(buffer)-1] == '\n')
+		buffer[strlen(buffer)-1] = '\0';
+	printf("Hi, %s!\n", buffer);
+}
+
+static void get_message(void)
+{
+	char buffer[48];
+
+	printf("Tell me a joke: ");
+	fgets(buffer, 256, stdin);
+}
+
+int main(void)
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	get_name();
+	get_message();
+	return 0;
+}
